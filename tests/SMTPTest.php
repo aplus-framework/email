aa577124f5e15d8fca86da76caccdd86bc6fc625
Natan Felles <natanfelles@gmail.com>
2019-11-07 14:55:26 -0300

    Test logs

--- tests/SMTPTest.php
+++ tests/SMTPTest.php
@@ -31,13 +31,75 @@ class SMTPTest extends TestCase
 		$this->assertEquals('utf-8', $this->smtp->getCharset());
 	}
 
+	protected function getMessage() : Message
+	{
+		return (new Message($this->smtp))
+			->addTo(\getenv('SMTP_ADDRESS'))
+			->setFrom(\getenv('SMTP_ADDRESS'))
+			->setPlainMessage('<b>Hello!</b>')
+			->setHTMLMessage('<b>Hello!</b>');
+	}
+
 	public function testSend()
 	{
-		$message = new Message($this->smtp);
-		$message->addTo(\getenv('SMTP_ADDRESS'));
-		$message->setFrom(\getenv('SMTP_ADDRESS'));
-		$message->setPlainMessage('<b>Hello!</b>');
-		$message->setHTMLMessage('<b>Hello!</b>');
-		$this->assertTrue($this->smtp->send($message));
+		$this->assertTrue($this->smtp->send($this->getMessage()));
+	}
+
+	public function testLogs()
+	{
+		$this->smtp->send($this->getMessage());
+		$this->assertEquals([
+			0 => '',
+			1 => '220 mailtrap.io ESMTP ready',
+		], $this->smtp->getLogs()[0]);
+		$this->assertEquals([
+			0 => 'EHLO ' . \gethostname(),
+			1 => '250-mailtrap.io
+250-SIZE 5242880
+250-PIPELINING
+250-ENHANCEDSTATUSCODES
+250-8BITMIME
+250-DSN
+250-AUTH PLAIN LOGIN CRAM-MD5
+250 STARTTLS',
+		], $this->smtp->getLogs()[1]);
+		$this->assertEquals([
+			0 => 'STARTTLS',
+			1 => '220 2.0.0 Start TLS',
+		], $this->smtp->getLogs()[2]);
+		$this->assertEquals([
+			0 => 'EHLO ' . \gethostname(),
+			1 => '250-mailtrap.io
+250-SIZE 5242880
+250-PIPELINING
+250-ENHANCEDSTATUSCODES
+250-8BITMIME
+250-DSN
+250 AUTH PLAIN LOGIN CRAM-MD5',
+		], $this->smtp->getLogs()[3]);
+		$this->assertEquals([
+			0 => 'AUTH LOGIN',
+			1 => '334 VXNlcm5hbWU6',
+		], $this->smtp->getLogs()[4]);
+		$this->assertEquals([
+			0 => 'M2YxZjlkNjdjYTFkYTU=',
+			1 => '334 UGFzc3dvcmQ6',
+		], $this->smtp->getLogs()[5]);
+		$this->assertEquals([
+			0 => 'YjdhNTIwYTViMDg5YmM=',
+			1 => '235 2.0.0 OK',
+		], $this->smtp->getLogs()[6]);
+		$this->assertEquals([
+			0 => 'MAIL FROM: <db20690ae8-23245c@inbox.mailtrap.io>',
+			1 => '250 2.1.0 Ok',
+		], $this->smtp->getLogs()[7]);
+		$this->assertEquals([
+			0 => 'RCPT TO: <db20690ae8-23245c@inbox.mailtrap.io>',
+			1 => '250 2.1.0 Ok',
+		], $this->smtp->getLogs()[8]);
+		$this->assertEquals([
+			0 => 'DATA',
+			1 => '354 Go ahead',
+		], $this->smtp->getLogs()[9]);
 	}
 }
