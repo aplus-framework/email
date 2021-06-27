698f972c725f4eaf65240b680ff221bdef42a278
Natan Felles <natanfelles@gmail.com>
2021-06-27 16:07:15 -0300

    Coding Standard: Add header comments

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Email;
+<?php
+/*
+ * This file is part of The Framework Email Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Email;
 
 use Framework\Email\SMTP;
 use PHPUnit\Framework\TestCase;
