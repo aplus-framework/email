4dc9f0497e5c8bf406e59d790475eba054fcf59c
Natan Felles <natanfelles@gmail.com>
2022-10-02 13:19:39 -0300

    Add Language attribute in Message::setHtmlMessage method

--- src/Message.php
+++ src/Message.php
@@ -10,6 +10,7 @@
 namespace Framework\Email;
 
 use DateTime;
+use JetBrains\PhpStorm\Language;
 use LogicException;
 
 /**
@@ -254,7 +255,7 @@ class Message implements \Stringable
      *
      * @return static
      */
-    public function setHtmlMessage(string $message) : static
+    public function setHtmlMessage(#[Language('HTML')] string $message) : static
     {
         $this->htmlMessage = $message;
         return $this;
