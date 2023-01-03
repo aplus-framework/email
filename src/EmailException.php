f9e2679b2d6e1b55be818627458de321c2608149
Natan Felles <natanfelles@gmail.com>
2023-01-03 17:05:22 -0300

    Add EmailException

---
+++ src/EmailException.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Email Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Email;
+
+/**
+ * Class EmailException.
+ *
+ * @package email
+ */
+class EmailException extends \RuntimeException
+{
+}
